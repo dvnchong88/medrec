67d268acd5764fd6c9f9b3a24bc18d8c657059c9
Devin Chong <dvnchong88@gmail.com>
2022-05-25 16:34:06 +0900

    fixed a mistake in the css

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2022_05_25_052232) do
 
   create_table "medical_records", force: :cascade do |t|
     t.bigint "patient_id", null: false
-    t.bigint "doctor_id"
+    t.bigint "doctor_id", null: false
     t.text "symptoms"
     t.text "diagnosis"
     t.string "prescribed_medicine"
