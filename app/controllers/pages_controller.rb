f1923a6397dcaecab4e8c8e3acc4a4c44cb1d3a7
Kyle Thomas <kylejwthomas@gmail.com>
2022-05-25 19:54:32 +0900

    homepage is pretty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,22 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :dashboard]
 
   def home
+    @hide_navbar = true
+    @hide_footer = true
   end
 
   def dashboard
-    @qr_code = RQRCode::QRCode.new("http://localhost:3000/")
-    @svg = @qr_code.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      standalone: true,
-      use_path: true
-    )
+    if user_signed_in? && current_user.user_type == "patient"
+      url = patient_medical_records_url(current_user.patient)
+
+      @qr_code = RQRCode::QRCode.new(patient_medical_records_url(current_user.patient))
+      @svg = @qr_code.as_svg(
+        offset: 0,
+        color: '000',
+        shape_rendering: 'crispEdges',
+        standalone: true,
+        use_path: true
+      )
+    end
   end
 end
