6d9b4fc7c35dc5397c5ae55b65a1329e1c096cac
Devin Chong <dvnchong88@gmail.com>
2022-05-26 16:55:39 +0900

    trying to debug heroku

--- db/migrate/20220525025408_fix_creator_in_medical_records.rb
+++ db/migrate/20220525025408_fix_creator_in_medical_records.rb
@@ -1,6 +1,5 @@
 class FixCreatorInMedicalRecords < ActiveRecord::Migration[6.1]
   def change
-    remove_column :medical_records, :creator
     add_column :medical_records, :creator, :integer
   end
 end
