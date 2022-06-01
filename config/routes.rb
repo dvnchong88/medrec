1f1a0854ddee118a2022142c834f2e47b715b616
Devin Chong <dvnchong88@gmail.com>
2022-06-01 15:59:53 +0900

    doctors can now see patients list

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/dashboard", to: "pages#dashboard", as: :dashboard
   resources :patients, only: [:show, :new, :create, :update, :edit, :index] do
-    resources :medical_records, only: [:index, :new, :create, :edit, :update, :destroy, :show]
+    resources :medical_records, only: [:index, :new, :create, :edit, :update, :destroy, :show, :search]
     member do
       patch "autofill"
     end
   end
-  resources :doctors, only: [:show, :new, :create, :update, :edit] do
+  resources :doctors, only: [:show, :new, :create, :update, :edit]
+  namespace :doctor do
     resources :medical_records, only: [:index, :new, :create, :edit, :update, :destroy, :show]
   end
   resources :events, only: [:index, :show, :new, :create, :update, :edit, :destroy]
