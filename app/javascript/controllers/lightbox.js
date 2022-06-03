e8bbdb55abd0cc83881cdd27ce8ed0e9ea09e30b
Devin Chong <dvnchong88@gmail.com>
2022-06-03 12:48:44 +0900

    added the pack tag

--- app/javascript/controllers/lightbox.js
+++ app/javascript/controllers/lightbox.js
@@ -1,29 +1,31 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "lightbox" ]
+  static targets = ["lightbox"]
   connect() {
-window.onload = () => {
-  // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
-  let all = document.getElementsByClassName("zoomD"),
-      lightbox = document.getElementById("lightbox");
+    window.onload = () => {
+      // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
+      let all = document.getElementsByClassName("zoomD"),
+        lightbox = document.getElementById("lightbox");
 
-  // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
-  // * SIMPLY CLONE INTO LIGHTBOX & SHOW
-  if (all.length>0) { for (let i of all) {
-    i.onclick = () => {
-      let clone = i.cloneNode();
-      clone.className = "";
-      lightbox.innerHTML = "";
-      lightbox.appendChild(clone);
-      lightbox.className = "show";
+      // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
+      // * SIMPLY CLONE INTO LIGHTBOX & SHOW
+      if (all.length > 0) {
+        for (let i of all) {
+          i.onclick = () => {
+            let clone = i.cloneNode();
+            clone.className = "";
+            lightbox.innerHTML = "";
+            lightbox.appendChild(clone);
+            lightbox.className = "show";
+          };
+        }
+      }
+
+      // (C) CLICK TO CLOSE LIGHTBOX
+      lightbox.onclick = () => {
+        lightbox.className = "";
+      };
     };
-  }}
-
-  // (C) CLICK TO CLOSE LIGHTBOX
-  lightbox.onclick = () => {
-    lightbox.className = "";
-  };
-};
-}
+  }
 }
